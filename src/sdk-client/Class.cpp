f7b0f4cc9ff7822e035fda58a6171e756c796c48
Pete Brown <psychlist1972@outlook.com>
2023-04-24 19:04:44 -0400

    New version of SDK

--- src/sdk-client/Class.cpp
+++ src/sdk-client/Class.cpp
@@ -2,7 +2,7 @@
 #include "Class.h"
 #include "Class.g.cpp"
 
-namespace winrt::Windows_Devices_Midi_Sdk::implementation
+namespace winrt::Microsoft::Devices::Midi2::implementation
 {
     int32_t Class::MyProperty()
     {
