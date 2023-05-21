48529f6f8acb85b736ba47ffd49debdd08d9441f
Pete Brown <psychlist1972@outlook.com>
2023-05-21 01:53:45 -0400

    Working on device information

--- get-started/midi-developers/app-developers/samples/cpp-winrt/sdk-client-basics/main.cpp
+++ get-started/midi-developers/app-developers/samples/cpp-winrt/sdk-client-basics/main.cpp
@@ -3,6 +3,8 @@
 // Further information: https://github.com/microsoft/MIDI/
 // ============================================================================
 
+// Windows MIDI Services sample code
+
 #include "pch.h"
 
 using namespace winrt;
@@ -18,23 +20,24 @@ int main()
     WindowsMidiServicesCheckError err;
     bool servicesOk = MidiServices::CheckForWindowsMidiServices(err);
 
-    // proceed only if MIDI services is present and compatible
-    // Your own application may decide to fall back to WinRT/WinMM MIDI 1.0 APIs, or
-    // to prompt the user to install the latest version of Windows MIDI Services
+    // proceed only if MIDI services is present and compatible. Your own application may decide
+    // to fall back to WinRT/WinMM MIDI 1.0 APIs, or to prompt the user to install the latest 
+    // version of Windows MIDI Services
 
     if (servicesOk)
     {
-        // create the MIDI session, giving us access to Windows MIDI Services
-        // An app may open more than one session. If so, the session name should
-        // be meaningful to the user, like the name of a browser tab, or a project.
+        // create the MIDI session, giving us access to Windows MIDI Services. An app may open 
+        // more than one session. If so, the session name should be meaningful to the user, like
+        // the name of a browser tab, or a project.
+
         auto session = MidiSession::CreateNewSession(L"Sample Session");
 
         hstring deviceSelector = MidiEndpoint::GetDeviceSelector();
 
         // Enumerate UMP endpoints. Note that per C++, main cannot be a co-routine,
         // so we can't just co_await this async call. We may end up wrapping this enumeration
-        // code into another class for that reason and also to instantly transform to 
-        // MidiDeviceInformation instances, to simplify calling code.
+        // code into another class to instantly transform to MidiDeviceInformation instances, 
+        // and to simplify calling code (reducing need for apps to incorporate async handling).
 
         Windows::Foundation::IAsyncOperation<DeviceInformationCollection> op = DeviceInformation::FindAllAsync(deviceSelector);
         DeviceInformationCollection endpointDevices = op.get();
