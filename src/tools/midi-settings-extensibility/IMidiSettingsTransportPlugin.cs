39abf9327d918c4f5d181713c5d7c9cb9d4bed00
Pete Brown <pete.brown@microsoft.com>
2023-06-14 18:52:57 -0400

    Settings app extensibility stub

--- src/tools/midi-settings-extensibility/IMidiSettingsTransportPlugin.cs
+++ src/tools/midi-settings-extensibility/IMidiSettingsTransportPlugin.cs
@@ -22,13 +22,13 @@ namespace Microsoft.Midi.Settings.Extensibility
 
         Microsoft.Devices.Midi2.MidiTransportInformation TransportInformation { get; }
 
-        // TODO: return a winui object
-        Windows.UI.Xaml.FrameworkElement GetTransportSettingsPanel();
+        //// TODO: return a winui object
+        //Windows.UI.Xaml.FrameworkElement GetTransportSettingsPanel();
 
 
-        // TODO: Have this take an endpoint info object and settings and return a winui object
-        Windows.UI.Xaml.FrameworkElement GetEndpointViewSettingsPanel(string settings);
-        Windows.UI.Xaml.FrameworkElement GetEndpointCreateSettingsPanel(string settings);
-        Windows.UI.Xaml.FrameworkElement GetEndpointUpdateSettingsPanel(string settings);
+        //// TODO: Have this take an endpoint info object and settings and return a winui object
+        //Windows.UI.Xaml.FrameworkElement GetEndpointViewSettingsPanel(string settings);
+        //Windows.UI.Xaml.FrameworkElement GetEndpointCreateSettingsPanel(string settings);
+        //Windows.UI.Xaml.FrameworkElement GetEndpointUpdateSettingsPanel(string settings);
     }
 }
