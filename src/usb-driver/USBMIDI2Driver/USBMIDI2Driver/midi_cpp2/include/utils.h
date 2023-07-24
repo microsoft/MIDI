a57c8b8341dd4293dd54edb8279337e337fca298
Michael Loh <90074657+AmeNote-Michael@users.noreply.github.com>
2023-07-24 10:57:01 -0600

    Created driver to USBMIDI2Driver
    Copied driver to USBMIDI2Driver from USB-UMPDriver to match desired naming.

---
+++ src/usb-driver/USBMIDI2Driver/USBMIDI2Driver/midi_cpp2/include/utils.h
@@ -0,0 +1,174 @@
+/**********************************************************
+ * MIDI 2.0 Library 
+ * Author: Andrew Mee
+ * 
+ * MIT License
+ * Copyright 2021 Andrew Mee
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * ********************************************************/
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cstdint>
+
+#define NOTE_OFF 0x80
+#define NOTE_ON 0x90
+#define KEY_PRESSURE 0xA0
+#define CC 0xB0
+#define RPN 0x20
+#define NRPN 0x30
+#define RPN_RELATIVE 0x40
+#define NRPN_RELATIVE 0x50
+#define PROGRAM_CHANGE 0xC0
+#define CHANNEL_PRESSURE 0xD0
+#define PITCH_BEND 0xE0
+#define PITCH_BEND_PERNOTE 0x60
+#define NRPN_PERNOTE 0x10
+#define RPN_PERNOTE 0x00
+#define PERNOTE_MANAGE 0xF0
+
+#define SYSEX_START 0xF0
+#define TIMING_CODE 0xF1
+#define SPP 0xF2
+#define SONG_SELECT 0xF3
+#define TUNEREQUEST 0xF6
+#define SYSEX_STOP 0xF7
+#define TIMINGCLOCK 0xF8
+#define SEQSTART 0xFA
+#define SEQCONT 0xFB
+#define SEQSTOP 0xFC
+#define ACTIVESENSE 0xFE
+#define SYSTEMRESET 0xFF
+
+#define UTILITY_NOOP 0x0
+#define UTILITY_JRCLOCK 0x1
+#define UTILITY_JRTS 0x2
+#define UTILITY_DELTACLOCKTICK 0x3
+#define UTILITY_DELTACLOCKSINCE 0x4
+
+#define FLEXDATA_COMMON 0x00
+#define FLEXDATA_COMMON_TEMPO 0x00
+#define FLEXDATA_COMMON_TIMESIG 0x01
+#define FLEXDATA_COMMON_METRONOME 0x02
+#define FLEXDATA_COMMON_KEYSIG 0x05
+#define FLEXDATA_COMMON_CHORD 0x06
+#define FLEXDATA_PERFORMANCE 0x01
+#define FLEXDATA_LYRIC 0x02
+
+#define MIDIENDPOINT 0x000
+#define MIDIENDPOINT_INFO_NOTIFICATION 0x001
+#define MIDIENDPOINT_DEVICEINFO_NOTIFICATION 0x002
+#define MIDIENDPOINT_NAME_NOTIFICATION 0x003
+#define MIDIENDPOINT_PRODID_NOTIFICATION 0x004
+#define MIDIENDPOINT_PROTOCOL_REQUEST 0x005
+#define MIDIENDPOINT_PROTOCOL_NOTIFICATION 0x006
+#define STARTOFSEQ 0x020
+#define ENDOFFILE 0x021
+
+#define FUNCTIONBLOCK 0x010
+#define FUNCTIONBLOCK_INFO_NOTFICATION 0x011
+#define FUNCTIONBLOCK_NAME_NOTIFICATION 0x012
+
+#ifndef S7_BUFFERLEN
+#define S7_BUFFERLEN	36
+#endif
+#define S7UNIVERSAL_NRT 0x7E
+#define S7UNIVERSAL_RT 0x7F
+#define S7MIDICI 0x0D
+
+#define MIDICI_DISCOVERY 0x70
+#define MIDICI_DISCOVERYREPLY 0x71
+#define MIDICI_ENDPOINTINFO 0x72
+#define MIDICI_ENDPOINTINFO_REPLY 0x73
+#define MIDICI_INVALIDATEMUID 0x7E
+#define MIDICI_ACK 0x7D
+#define MIDICI_NAK 0x7F
+
+#define MIDICI_PROTOCOL_NEGOTIATION 0x10
+#define MIDICI_PROTOCOL_NEGOTIATION_REPLY 0x11
+#define MIDICI_PROTOCOL_SET 0x12
+#define MIDICI_PROTOCOL_TEST 0x13
+#define MIDICI_PROTOCOL_TEST_RESPONDER 0x14
+#define MIDICI_PROTOCOL_CONFIRM 0x15
+
+#define MIDICI_PROFILE_INQUIRY 0x20
+#define MIDICI_PROFILE_INQUIRYREPLY 0x21
+#define MIDICI_PROFILE_SETON 0x22
+#define MIDICI_PROFILE_SETOFF 0x23
+#define MIDICI_PROFILE_ENABLED 0x24
+#define MIDICI_PROFILE_DISABLED 0x25
+#define MIDICI_PROFILE_ADD 0x26
+#define MIDICI_PROFILE_REMOVE 0x27
+#define MIDICI_PROFILE_DETAILS_INQUIRY 0x28
+#define MIDICI_PROFILE_DETAILS_REPLY 0x29
+#define MIDICI_PROFILE_SPECIFIC_DATA 0x2F
+
+#define MIDICI_PE_CAPABILITY 0x30
+#define MIDICI_PE_CAPABILITYREPLY 0x31
+#define MIDICI_PE_GET 0x34
+#define MIDICI_PE_GETREPLY 0x35
+#define MIDICI_PE_SET 0x36
+#define MIDICI_PE_SETREPLY 0x37
+#define MIDICI_PE_SUB 0x38
+#define MIDICI_PE_SUBREPLY 0x39
+#define MIDICI_PE_NOTIFY 0x3F
+
+#define MIDICI_PI_CAPABILITY 0x40
+#define MIDICI_PI_CAPABILITYREPLY 0x41
+#define MIDICI_PI_MM_REPORT 0x42
+#define MIDICI_PI_MM_REPORT_REPLY 0x43
+#define MIDICI_PI_MM_REPORT_END 0x44
+
+#define MIDICI_PE_COMMAND_START 1
+#define MIDICI_PE_COMMAND_END 2
+#define MIDICI_PE_COMMAND_PARTIAL 3
+#define MIDICI_PE_COMMAND_FULL 4
+#define MIDICI_PE_COMMAND_NOTIFY 5
+
+#define MIDICI_PE_ACTION_COPY 1
+#define MIDICI_PE_ACTION_MOVE 2
+#define MIDICI_PE_ACTION_DELETE 3
+#define MIDICI_PE_ACTION_CREATE_DIR 4
+
+#define MIDICI_PE_ASCII 1
+#define MIDICI_PE_MCODED7 2
+#define MIDICI_PE_MCODED7ZLIB 3
+
+#define FUNCTION_BLOCK 0x7F
+#define M2_CI_BROADCAST 0xFFFFFFF
+
+#define UMP_VER_MAJOR	1
+#define UMP_VER_MINOR	1
+
+#ifndef EXP_MIDICI_PE_EXPERIMENTAL_PATH
+#define EXP_MIDICI_PE_EXPERIMENTAL_PATH 1
+#endif
+
+#define UMP_UTILITY 0x0
+#define UMP_SYSTEM 0x1
+#define UMP_M1CVM 0x2
+#define UMP_SYSEX7 0x3
+#define UMP_M2CVM 0x4
+#define UMP_DATA 0x5
+#define UMP_FLEX_DATA 0xD
+#define UMP_MIDI_ENDPOINT 0xF
+
+namespace M2Utils {
+uint32_t scaleUp(uint32_t srcVal, uint8_t srcBits, uint8_t dstBits);
+
+uint32_t scaleDown(uint32_t srcVal, uint8_t srcBits, uint8_t dstBits);
+
+}
+#endif
---
+++ src/usb-driver/USBMIDI2Driver/USBMIDI2Driver/midi_cpp2/src/bytestreamToUMP.cpp
@@ -0,0 +1,223 @@
+/**********************************************************
+ * MIDI 2.0 Library 
+ * Author: Andrew Mee
+ * 
+ * MIT License
+ * Copyright 2021 Andrew Mee
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * ********************************************************/
+
+
+#include "../include/utils.h"
+#include "../include/bytestreamToUMP.h"
+
+bytestreamToUMP::bytestreamToUMP(){
+	memset(bankMSB, 255, sizeof(bankMSB));
+	memset(bankLSB, 255, sizeof(bankLSB));
+	memset(rpnMsbValue, 255, sizeof(rpnMsbValue));
+	memset(rpnMsb, 255, sizeof(rpnMsb));
+	memset(rpnLsb, 255, sizeof(rpnLsb));
+}
+	 
+void bytestreamToUMP::bsToUMP(uint8_t b0, uint8_t b1, uint8_t b2){
+  uint8_t status = b0 & 0xF0;
+ 
+   if(d0 >= TIMING_CODE){
+	  umpMess[messPos] = ((UMP_SYSTEM << 4) + defaultGroup + 0L) << 24;
+	  umpMess[messPos] +=  (b0 + 0L) << 16;
+	  umpMess[messPos] +=  b1  << 8;
+	  umpMess[messPos] +=  b2;
+	  messPos++;
+  }else if(status>=NOTE_OFF && status<=PITCH_BEND){
+	  if(outputMIDI2){
+		  uint8_t channel = b0 & 0xF;
+		  
+		  if(status==NOTE_ON && b2==0){
+			 status=NOTE_OFF;
+		  }
+		  
+		  umpMess[messPos] = ((UMP_M2CVM << 4) + defaultGroup + 0L) << 24;
+		  umpMess[messPos] += (status + channel + 0L)<<16;
+		  
+		  if(status==NOTE_ON || status==NOTE_OFF){
+			umpMess[messPos] += (b1 + 0L) <<8;
+                        umpMess[messPos+1] = (M2Utils::scaleUp(b2,7,16) << 16);
+		  } else if (status == KEY_PRESSURE){
+			umpMess[messPos] += (b1 + 0L) <<8;
+                        umpMess[messPos+1] = M2Utils::scaleUp(b2,7,32);
+		  } else if (status == PITCH_BEND){
+                        umpMess[messPos+1] = M2Utils::scaleUp((b1<<7) + b2,14,32);
+		  } else if (status == PROGRAM_CHANGE){
+			if(bankMSB[channel]!=255 && bankLSB[channel]!=255){
+				umpMess[messPos] += 1;
+				umpMess[messPos+1] += (bankMSB[channel] <<8) + bankLSB[channel ];
+			}
+			umpMess[messPos+1] += (b2 + 0L) << 24;
+		  } else if (status == CHANNEL_PRESSURE){
+                        umpMess[messPos+1] = M2Utils::scaleUp(b1,7,32);
+		  }  else if (status == CC){
+			switch(b1){
+			 case 0:
+				bankMSB[channel] = b2;
+				return;
+			 case 32:
+				bankLSB[channel] = b2;
+				return; 
+			  
+			 case 6: //RPN MSB Value
+				if(rpnMsb[channel]==255 || rpnLsb[channel]==255){
+					return;
+				}
+				
+				if(rpnMode[channel] && rpnMsb[channel] == 0 && (rpnLsb[channel] == 0 || rpnLsb[channel] == 6)){
+					status = rpnMode[channel]? RPN: NRPN;
+					
+					umpMess[messPos] = ((UMP_M2CVM << 4) + defaultGroup + 0L) << 24;
+					umpMess[messPos] += (status + channel + 0L)<<16;
+					umpMess[messPos] += ((int)rpnMsb[channel]<<7) + rpnLsb[channel] + 0L;
+                                    umpMess[messPos+1] = M2Utils::scaleUp(((int)b2<<7),14,32);
+
+				}else{
+					rpnMsbValue[channel] = b2;
+					return;
+				}
+				break;
+			case 38: //RPN LSB Value
+				if(rpnMsb[channel]==255 || rpnLsb[channel]==255){
+					return;
+				}
+				status = rpnMode[channel]? RPN: NRPN;
+				
+				umpMess[messPos] = ((UMP_M2CVM << 4) + defaultGroup + 0L) << 24;
+				umpMess[messPos] += (status  + channel + 0L)<<16;
+				umpMess[messPos] += ((int)rpnMsb[channel]<<7) + rpnLsb[channel] + 0L;
+                                umpMess[messPos+1] = M2Utils::scaleUp(((int)rpnMsbValue[channel]<<7) + b2,14,32);
+				break;
+			case 99:
+				rpnMode[channel] = false;
+				rpnMsb[channel] = b2;
+				return;	
+			case 98:
+				rpnMode[channel] = false;
+				rpnLsb[channel] = b2;
+				return;
+			case 101:
+				rpnMode[channel] = true;
+				rpnMsb[channel] = b2;
+				return;	
+				
+			case 100:
+				rpnMode[channel] = true;
+				rpnLsb[channel] = b2;
+				return;
+					
+			default:
+				umpMess[messPos] += (b1 + 0L) <<8;
+                                umpMess[messPos+1] = M2Utils::scaleUp(b2,7,32);
+				break;
+			}					
+		  }
+		  messPos+=2;
+		  
+	  }	else {
+		  umpMess[messPos] = ((UMP_M1CVM << 4) + defaultGroup + 0L) << 24;        
+		  umpMess[messPos] +=  (b0 + 0L) << 16;
+		  umpMess[messPos] +=  b1  << 8;
+		  umpMess[messPos] +=  b2;
+		  
+		  messPos++;
+	  }
+  }
+  
+}
+
+
+bool bytestreamToUMP::availableUMP(){
+	return messPos;
+}
+
+uint32_t bytestreamToUMP::readUMP(){
+	uint32_t mess = umpMess[0];			
+	for(uint8_t i=0;i<messPos;i++){
+		umpMess[i]=umpMess[i+1];
+	}
+	messPos--;			
+	
+	return mess;
+}
+
+
+void bytestreamToUMP::bytestreamParse(uint8_t midi1Byte){
+		  
+  if (midi1Byte == TUNEREQUEST || midi1Byte >=  TIMINGCLOCK) { 
+	  bsToUMP(midi1Byte,0,0);
+	  return;
+  }
+  
+  if (midi1Byte & NOTE_OFF) { // Status byte received
+	d0 = midi1Byte;
+	d1 = 255;
+	
+	if (midi1Byte == SYSEX_START){
+		sysex7State = 1;
+		sysex7Pos = 0;
+	}else
+	if (midi1Byte == SYSEX_STOP){
+	  
+		umpMess[messPos] = ((UMP_SYSEX7 << 4) + defaultGroup + 0L) << 24;
+		umpMess[messPos] +=  ((sysex7State == 1?0:3) + 0L) << 20;
+		umpMess[messPos] +=  ((sysex7Pos + 0L) << 16) ;
+		umpMess[messPos++] += (sysex[0] << 8) + sysex[1];
+		umpMess[messPos++] = ((sysex[2] + 0L) << 24) + ((sysex[3] + 0L)<< 16) + (sysex[4] << 8) + sysex[5];
+
+		sysex7State = 0;
+		memset(sysex, 0, sizeof(sysex));
+	}
+  } else 
+  if(sysex7State >= 1){
+	  //Check IF new UMP Message Type 3
+	  if(sysex7Pos%6 == 0 && sysex7Pos !=0){
+		 umpMess[messPos] = ((UMP_SYSEX7 << 4) + defaultGroup + 0L) << 24;
+		 umpMess[messPos] +=  (sysex7State + 0L) << 20;
+		 umpMess[messPos] +=  6L << 16;
+		 umpMess[messPos++] += (sysex[0] << 8) + sysex[1];
+		 umpMess[messPos++] = ((sysex[2] + 0L) << 24) + ((sysex[3] + 0L)<< 16) + (sysex[4] << 8) + sysex[5] + 0L;
+		 memset(sysex, 0, sizeof(sysex));
+		 sysex7State=2;
+		 sysex7Pos=0;
+	  }
+	  
+	  sysex[sysex7Pos] = midi1Byte;
+	  sysex7Pos++;
+  } else
+  if (d1 != 255) { // Second byte
+      bsToUMP(d0, d1, midi1Byte);
+		d1 = 255;
+  } else if (d0){ // status byte set
+	  if (
+		d0 == PROGRAM_CHANGE
+		|| d0 == TIMING_CODE
+		|| d0 == CHANNEL_PRESSURE
+		|| d0 == SONG_SELECT
+	  ) { 
+          bsToUMP(d0, midi1Byte, 0);
+		  return;
+	  } else 
+	  if (d0 < SYSEX_START || d0 == SPP) { // First data byte
+		d1=midi1Byte;
+		return;
+	  }
+  }  
+}
+
