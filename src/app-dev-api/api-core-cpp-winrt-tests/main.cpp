f82e028a1b3750550f93ce747ea5739322a97063
Pete Brown <pete.brown@microsoft.com>
2023-08-12 16:29:20 -0400

    Update main.cpp

--- src/app-dev-api/api-core-cpp-winrt-tests/main.cpp
+++ src/app-dev-api/api-core-cpp-winrt-tests/main.cpp
@@ -1,13 +1,20 @@
 ﻿#include "pch.h"
 
 //#define CATCH_CONFIG_MAIN
+//#define CATCH_CONFIG_RUNNER
+#define CATCH_AMALGAMATED_CUSTOM_MAIN
+
 
 #include "catch_amalgamated.hpp"
+
+#include <iostream>
 
 using namespace winrt::Windows::Foundation;
 
 int main(int argc, char* argv[]) 
 {
+    std::cout << "Custom main for Catch2" << std::endl;
+
     winrt::init_apartment();
 
 
