e3ee37d62fa43ad815f2d514131dafb19baf020d
Pete Brown <pete.brown@microsoft.com>
2023-08-26 21:34:27 -0400

    Update MessageBuilderTests.cpp

--- src/api/Test/Midi2.Api.unittests/MessageBuilderTests.cpp
+++ src/api/Test/Midi2.Api.unittests/MessageBuilderTests.cpp
@@ -14,47 +14,47 @@
 using namespace winrt::Windows::Devices::Midi2;
 
 
-TEST_CASE("Build Type 0 Utility Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type 1 System Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type 2 MIDI 1.0 Channel Voice Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type 3 SysEx7 Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type 4 MIDI 2.0 Channel Voice Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type 5 SysEx8 Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type 5 Mixed Data Set Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type D Flex Data Messages")
-{
-    REQUIRE(false);
-}
-
-TEST_CASE("Build Type F Stream Messages")
-{
-    REQUIRE(false);
-}
+//TEST_CASE("Build Type 0 Utility Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type 1 System Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type 2 MIDI 1.0 Channel Voice Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type 3 SysEx7 Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type 4 MIDI 2.0 Channel Voice Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type 5 SysEx8 Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type 5 Mixed Data Set Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type D Flex Data Messages")
+//{
+//    REQUIRE(false);
+//}
+//
+//TEST_CASE("Build Type F Stream Messages")
+//{
+//    REQUIRE(false);
+//}
