3dc0d5ad6ac75ee16abc0a5bed314f75bd625f3c
Pete Brown <pete.brown@microsoft.com>
2023-12-22 17:19:24 -0500

    Start of unit/isolation test for scheduler

---
+++ src/api/Test/Midi2.Transform.unittests/MidiSchedulerTransformTests.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+#include "stdafx.h"
+#include "Midi2SchedulerTransform.h"
+#include "Midi2MidiSrvAbstraction.h"
+
+#include "MidiSchedulerTransformTests.h"
+
+#include <initguid.h>
+#include "MidiDefs.h"
+
+
---
+++ src/api/Test/Midi2.Transform.unittests/stdafx.h
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+#pragma once
+
+#include <windows.h>
+#include <cguid.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Devices.Enumeration.h>
+#include <ks.h>
+#include <avrt.h>
+#include <wil\com.h>
+#include <wil\resource.h>
+#include <wil\result_macros.h>
+#include <ppltasks.h>
+#include <WexTestClass.h>
+
+#include "MidiDeviceManagerInterface.h"
+#include "MidiTestCommon.h"
+
