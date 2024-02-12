cb1aa52136557cb237c16840c93d772bfe8f22dc
Pete Brown <pete.brown@microsoft.com>
2024-02-12 14:24:56 -0500

    Fix header comments

--- src/api/Inc/loopback_ids.h
+++ src/api/Inc/loopback_ids.h
@@ -1,3 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License
+// ============================================================================
+// This is part of the Windows MIDI Services App API and should be used
+// in your Windows application via an official binary distribution.
+// Further information: https://github.com/microsoft/MIDI/
+// ============================================================================
+
 #pragma once
 
 #define MIDI_DIAGNOSTICS_LOOPBACK_BIDI_ID_A L"\\\\?\\SWD#MIDISRV#MIDIU_DIAG_LOOPBACK_A#{e7cce071-3c03-423f-88d3-f1045d02552b}"
