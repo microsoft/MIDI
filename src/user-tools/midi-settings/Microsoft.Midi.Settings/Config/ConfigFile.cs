b09e53ba4807d941313c9de4835f86d6a26948e1
Pete Brown <pete.brown@microsoft.com>
2024-04-14 13:36:12 -0400

    Settings app updates

---
+++ src/user-tools/midi-settings/Microsoft.Midi.Settings/Config/ConfigFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Data.Json;
+using System.Threading.Tasks;
+
+namespace Microsoft.Midi.Settings.Config
+{
+    // this code really should be refactored into a shared C++/WinRT lib used in the service and here
+
+
+    internal class ConfigFile
+    {
+        private JsonObject _jsonObject;
+
+        //public JsonObject GetEndpointTransportPluginSettingsForTransport(Guid transportId)
+        //{
+            
+
+        //}
+
+
+
+
+
+
+    }
+}
