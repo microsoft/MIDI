d22d21fce8940750b478e0334e13d375c8a2ca77
Pete Brown <pete.brown@microsoft.com>
2024-05-07 22:43:55 -0400

    Update replace_running_service_x64.bat

--- build/replace_running_service_x64.bat
+++ build/replace_running_service_x64.bat
@@ -4,6 +4,7 @@ echo This will only replace the github-built Windows service and components
 echo If you are using the in-box service or dism-deployed service, this will not work.
 echo This must be run as administrator.
 
+echo Stopping midisrv so we can replace it
 sc stop midisrv
 
 set servicepath="%ProgramFiles%\Windows MIDI Services\Service"
@@ -11,21 +12,19 @@ set apipath="%ProgramFiles%\Windows MIDI Services\API"
 set dmppath="%ProgramFiles%\Windows MIDI Services\"
 set buildoutput="%midi_repo_root%src\api\VSFiles\x64\Release"
 
-echo mididmp.exe
-copy /Y %buildoutput%\mididmp.exe %dmppath%
-
-echo MidiSrv.exe
+echo MidiSrv.exe and related dlls
 copy /Y %buildoutput%\MidiSrv.exe %servicepath%
 copy /Y %buildoutput%\Midi2.*Abstraction.dll %servicepath%
 copy /Y %buildoutput%\Midi2.*Transform.dll %servicepath%
 
-echo API impl
-copy /Y %buildoutput%\*.Devices.Midi2.dll %apipath%
-echo API pri
-copy /Y %buildoutput%\*.Devices.Midi2.pri %apipath%
+REM echo mididmp.exe
+REM copy /Y %buildoutput%\mididmp.exe %dmppath%
 
+REM echo API impl
+REM copy /Y %buildoutput%\*.Devices.Midi2.dll %apipath%
+REM echo API pri
+REM copy /Y %buildoutput%\*.Devices.Midi2.pri %apipath%
 
-
-sc start midisrv
+REM sc start midisrv
 
 pause
