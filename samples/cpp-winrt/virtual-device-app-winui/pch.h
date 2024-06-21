536884f73ab686b3650467cc91968d5b7e8fce58
Pete Brown <pete.brown@microsoft.com>
2024-06-21 17:51:59 -0400

    Fix CPP WinRT virtual device sample

--- samples/cpp-winrt/virtual-device-app-winui/pch.h
+++ samples/cpp-winrt/virtual-device-app-winui/pch.h
@@ -11,11 +11,13 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
+
 #include <winrt/Microsoft.UI.Composition.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Microsoft.UI.Xaml.Data.h>
+#include <winrt/Microsoft.UI.Xaml.Input.h>
 #include <winrt/Microsoft.UI.Xaml.Interop.h>
 #include <winrt/Microsoft.UI.Xaml.Markup.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
