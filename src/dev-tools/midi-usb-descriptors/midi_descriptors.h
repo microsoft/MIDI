eef298c9281fca7653f4a201cb70c55268930d7f
Pete Brown <pete.brown@microsoft.com>
2024-10-07 14:59:23 -0400

    Start of MIDI 2.0 USB descriptor tool

---
+++ src/dev-tools/midi-usb-descriptors/midi_descriptors.h
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License
+// ============================================================================
+// This is part of the Windows MIDI Services App SDK and should be used
+// in your Windows application via an official binary distribution.
+// Further information: https://aka.ms/midi
+// ============================================================================
+// Portions of this code adapted from the Windows UsbView Sample application
+// Licenses and restrictions for that code apply
+
+
+#pragma once
+
