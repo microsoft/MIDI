e6737e52cf486be9e3893bec4b8e01e89f80699b
Pete Brown <pete.brown@microsoft.com>
2024-10-16 22:45:52 -0400

    Remove old SDK projects

--- src/app-sdk/winrt-endpoint-ble/pch.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License
-// ============================================================================
-// This is part of the Windows MIDI Services App API and should be used
-// in your Windows application via an official binary distribution.
-// Further information: https://aka.ms/midi
-// ============================================================================
-
-#include "pch.h"
