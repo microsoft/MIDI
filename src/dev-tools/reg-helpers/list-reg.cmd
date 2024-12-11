cabfb04a1d1331b40693ddcc4a39ccfc8bd49f24
Pete Brown <pete.brown@microsoft.com>
2024-12-11 12:02:32 -0500

    Update dev prep scripts to unblock downloaded content

--- src/dev-tools/reg-helpers/list-reg.cmd
+++ src/dev-tools/reg-helpers/list-reg.cmd
@@ -1,1 +1,14 @@
+@echo off
+echo[
+echo ==================================================================================
+echo To run this script, you must have the ability to run local scripts turned on. You 
+echo can find this in Settings - System - For Developers - Powershell
+echo[
+echo You also need to run this from an elevated command prompt. Do not just double
+echo click this cmd file.
+echo ==================================================================================
+echo[
+
+pwsh -Command Unblock-File .\midi-list-reg.ps1
+
 pwsh .\midi-list-reg.ps1
