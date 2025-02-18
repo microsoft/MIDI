d9eee2ae2d61058fea333b682bfb504a9dda3908
Pete Brown <pete.brown@microsoft.com>
2025-02-18 00:22:05 -0500

    wdmaud2.drv contiguous port ordering workaround

--- build/staging/version/WindowsMidiServicesVersion.cs
+++ build/staging/version/WindowsMidiServicesVersion.cs
@@ -6,12 +6,12 @@ namespace Microsoft.Midi.Common
 	{
 		public const string Source = "GitHub Preview";
 		public const string Name = "Customer Preview 2";
-		public const string BuildFullVersion = "1.0.3-preview-11.250217-1932";
+		public const string BuildFullVersion = "1.0.3-preview-11.250218-5";
 		public const string VersionMajor = "1";
 		public const string VersionMinor = "0";
 		public const string VersionRevision = "3";
-		public const string VersionDateNumber = "250217";
-		public const string VersionTimeNumber = "1932";
+		public const string VersionDateNumber = "250218";
+		public const string VersionTimeNumber = "5";
 	}
 }
 
