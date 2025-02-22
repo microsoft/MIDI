a5a73745d2bee69cdbb23b9a53b568a989b60a4c
Pete Brown <pete.brown@microsoft.com>
2025-02-22 16:13:21 -0500

    Create IMidiServiceRegistrySettingsService.cs

---
+++ src/user-tools/midi-settings/Microsoft.Midi.Settings/Contracts/Services/IMidiServiceRegistrySettingsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Midi.Settings.Contracts.Services
+{
+    public interface IMidiServiceRegistrySettingsService
+    {
+        bool GetDefaultToOldMidi1PortNaming();
+        bool SetDefaultToOldMidi1PortNaming(bool newValue);
+
+
+        bool GetUseMMCSS();
+        bool SetUseMMCSS(bool newValue);
+
+
+        bool GetMidi2DiscoveryEnabled();
+        bool SetMidi2DiscoveryEnabled(bool newValue);
+
+
+        UInt32 GetMidi2DiscoveryTimeoutMS();
+        bool SetMidi2DiscoveryTimeoutMS(UInt32 newValue);
+
+    }
+}
