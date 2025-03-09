aa3e3e102234a94f40f51659a9ec6fd88039b769
Pete Brown <pete.brown@microsoft.com>
2025-03-09 00:05:10 -0500

    C++ sample showing how to detect the service

---
+++ samples/cpp/detectmidi/pch.h
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License
+// ============================================================================
+// This is part of the Windows MIDI Services App SDK and should be used
+// in your Windows application via an official binary distribution.
+// Further information: https://aka.ms/midi
+// ============================================================================
+
+
+#pragma once
+
+#pragma warning (push)
+#pragma warning (disable: 4005)
+
+#include <windows.h>
+//#include <winternl.h>
+
+//#include <ntstatus.h>
+
+#include <iostream>
+#include <chrono>
+#include <format>
+
+
+#include <wrl\module.h>
+#include <wrl\event.h>
+#include <wil\com.h>
+#include <wil\resource.h>
+#include <wil\result_macros.h>
+#include <wil\tracelogging.h>
+
+#include <wil\com.h>
+#include <wil\resource.h>
+#include <wil\result_macros.h>
+
+#include <mmeapi.h>
+
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlctl.h>
+#include <atlcoll.h>
+#include <atlsync.h>
+
+#include <atlconv.h>
+#include <string>
+
+#include <winmeta.h>
+#include <TraceLoggingProvider.h>
+
+//#include "SWDevice.h"
+#include <initguid.h>
+//#include "Devpkey.h"
+
+#pragma warning (pop)
