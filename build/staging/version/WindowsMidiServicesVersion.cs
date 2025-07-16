5e58a97a5dbfdd02151c6798e80c8c26a428e90e
Pete Brown <pete.brown@microsoft.com>
2025-07-16 13:13:05 -0400

    Build artifacts

--- build/staging/version/WindowsMidiServicesVersion.cs
+++ build/staging/version/WindowsMidiServicesVersion.cs
@@ -9,15 +9,15 @@ namespace Microsoft.Windows.Devices.Midi2.Common
 	{
 		public const bool IsPreview = true;
 		public const string Source = "GitHub Preview";
-		public const string BuildDate = "2025-07-15";
+		public const string BuildDate = "2025-07-16";
 		public const string Name = "Preview 11";
-		public const string BuildFullVersion = "1.0.10-preview.11.1731";
+		public const string BuildFullVersion = "1.0.10-preview.11.1732";
 		public const ushort VersionMajor = 1;
 		public const ushort VersionMinor = 0;
 		public const ushort VersionPatch = 10;
-		public const string Preview = "preview.11.1731";
-		public const string AssemblyFullVersion = "1.0.10.1731";
-		public const string FileFullVersion = "1.0.10.1731";
+		public const string Preview = "preview.11.1732";
+		public const string AssemblyFullVersion = "1.0.10.1732";
+		public const string FileFullVersion = "1.0.10.1732";
 	}
 }
 
