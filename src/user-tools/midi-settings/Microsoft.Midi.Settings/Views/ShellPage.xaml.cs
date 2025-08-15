e97a2786f4bf893e1c8ea3ecb8dd7ab44be5c55d
Pete Brown <pete.brown@microsoft.com>
2025-08-15 11:28:15 -0400

    Titlebar updates to Settings app

--- src/user-tools/midi-settings/Microsoft.Midi.Settings/Views/ShellPage.xaml.cs
+++ src/user-tools/midi-settings/Microsoft.Midi.Settings/Views/ShellPage.xaml.cs
@@ -36,15 +36,15 @@ public sealed partial class ShellPage : Page
         ViewModel.NavigationService.Frame = NavigationFrame;
         ViewModel.NavigationViewService.Initialize(NavigationViewControl);
 
-        App.MainWindow.ExtendsContentIntoTitleBar = true;
-        App.MainWindow.SetTitleBar(AppTitleBar);
+       // App.MainWindow.ExtendsContentIntoTitleBar = true;
+       // App.MainWindow.SetTitleBar(AppTitleBar);
         App.MainWindow.Activated += MainWindow_Activated;
 //        AppTitleBarText.Text = "AppDisplayName".GetLocalized();
     }
 
     private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-        TitleBarHelper.UpdateTitleBar(RequestedTheme);
+    //    TitleBarHelper.UpdateTitleBar(RequestedTheme);
 
         KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu));
         KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.GoBack));
@@ -93,13 +93,13 @@ public sealed partial class ShellPage : Page
 
     private void NavigationViewControl_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
     {
-        AppTitleBar.Margin = new Thickness()
-        {
-            Left = sender.CompactPaneLength * (sender.DisplayMode == NavigationViewDisplayMode.Minimal ? 2 : 1),
-            Top = AppTitleBar.Margin.Top,
-            Right = AppTitleBar.Margin.Right,
-            Bottom = AppTitleBar.Margin.Bottom
-        };
+        //AppTitleBar.Margin = new Thickness()
+        //{
+        //    Left = sender.CompactPaneLength * (sender.DisplayMode == NavigationViewDisplayMode.Minimal ? 2 : 1),
+        //    Top = AppTitleBar.Margin.Top,
+        //    Right = AppTitleBar.Margin.Right,
+        //    Bottom = AppTitleBar.Margin.Bottom
+        //};
     }
 
     private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
