dc94b47b20ff39aab0342ad2cae724b1e1fc95c9
Pete Brown <pete.brown@microsoft.com>
2025-09-24 00:04:11 -0400

    Build artifacts

--- build/staging/version/WindowsMidiServicesVersion.cs
+++ build/staging/version/WindowsMidiServicesVersion.cs
@@ -11,13 +11,13 @@ namespace Microsoft.Windows.Devices.Midi2.Common
 		public const string Source = "GitHub Preview";
 		public const string BuildDate = "2025-09-23";
 		public const string Name = "Service Preview 14";
-		public const string BuildFullVersion = "1.0.15-preview.14.1";
+		public const string BuildFullVersion = "1.0.15-preview.14.5";
 		public const ushort VersionMajor = 1;
 		public const ushort VersionMinor = 0;
 		public const ushort VersionPatch = 15;
-		public const ushort VersionBuildNumber = 1;
-		public const string Preview = "preview.14.1";
-		public const string AssemblyFullVersion = "1.0.15.1";
-		public const string FileFullVersion = "1.0.15.1";
+		public const ushort VersionBuildNumber = 5;
+		public const string Preview = "preview.14.5";
+		public const string AssemblyFullVersion = "1.0.15.5";
+		public const string FileFullVersion = "1.0.15.5";
 	}
 }
--- build/staging/version/WindowsMidiServicesVersion.h
+++ build/staging/version/WindowsMidiServicesVersion.h
@@ -9,12 +9,12 @@
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_SOURCE                             L"GitHub Preview"
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_DATE                               L"2025-09-23"
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_NAME                       L"Service Preview 14"
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FULL                       L"1.0.15-preview.14.1"
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FULL                       L"1.0.15-preview.14.5"
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_MAJOR                      1
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_MINOR                      0
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_PATCH                      15
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_BUILD_NUMBER               1
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_PREVIEW                            L"preview.14.1"
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FILE                       L"1.0.15.1"
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_BUILD_NUMBER               5
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_PREVIEW                            L"preview.14.5"
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FILE                       L"1.0.15.5"
 
 #endif
