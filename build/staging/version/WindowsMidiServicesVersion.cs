5cbbd828529b1fb4f40a5cfed268ca4d96dcab04
Pete Brown <pete.brown@microsoft.com>
2025-11-12 22:30:18 -0500

    Update settings app to .NET 10 and VS 2026

--- build/staging/version/WindowsMidiServicesVersion.cs
+++ build/staging/version/WindowsMidiServicesVersion.cs
@@ -11,13 +11,13 @@ namespace Microsoft.Windows.Devices.Midi2.Common
 		public const string Source = "GitHub Preview";
 		public const string BuildDate = "2025-11-12";
 		public const string Name = "SDK Release Candidate 1";
-		public const string BuildFullVersion = "1.0.14-rc.1.161";
+		public const string BuildFullVersion = "1.0.14-rc.1.163";
 		public const ushort VersionMajor = 1;
 		public const ushort VersionMinor = 0;
 		public const ushort VersionPatch = 14;
-		public const ushort VersionBuildNumber = 161;
-		public const string Preview = "rc.1.161";
-		public const string AssemblyFullVersion = "1.0.14.161";
-		public const string FileFullVersion = "1.0.14.161";
+		public const ushort VersionBuildNumber = 163;
+		public const string Preview = "rc.1.163";
+		public const string AssemblyFullVersion = "1.0.14.163";
+		public const string FileFullVersion = "1.0.14.163";
 	}
 }
--- build/staging/version/WindowsMidiServicesVersion.h
+++ build/staging/version/WindowsMidiServicesVersion.h
@@ -9,12 +9,12 @@
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_SOURCE                             L"GitHub Preview"
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_DATE                               L"2025-11-12"
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_NAME                       L"SDK Release Candidate 1"
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FULL                       L"1.0.14-rc.1.161"
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FULL                       L"1.0.14-rc.1.163"
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_MAJOR                      1
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_MINOR                      0
 #define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_PATCH                      14
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_BUILD_NUMBER               161
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_PREVIEW                            L"rc.1.161"
-#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FILE                       L"1.0.14.161"
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_BUILD_NUMBER               163
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_PREVIEW                            L"rc.1.163"
+#define WINDOWS_MIDI_SERVICES_NUGET_BUILD_VERSION_FILE                       L"1.0.14.163"
 
 #endif
