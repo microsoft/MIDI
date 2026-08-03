1596b1446e039abae745a7431c1ec85d0f0413f9
Pete Brown <pete.brown@microsoft.com>
2026-08-03 00:35:50 -0400

    Add SysEx file send and legacy port watcher to MIDI COnsole
    - Uncomment and implement EndpointSendSysExFileCommand with progress bar, using MidiBytestreamToUmpMessageConverterState and MidiSystemExclusiveSender
    - Add new WatchPortsCommand for monitoring MIDI 1.0 legacy ports
    - Add watch-ports CLI command with watch-legacy alias
    - Add watch-ump alias to watch-endpoints command
    - Update UMP endpoint watcher notification strings to include 'UMP' prefix
    - Add new localized strings for legacy port watcher and SysEx file parameters
    - Bump NuGet package reference to 0.99.36-devpreview.4 and nuspec version to 0.99.37-devpreview.4
    - Add global using for Windows.Devices.Midi2.Utilities.SysExTransfer

--- src/user-tools/midi-console/Midi/Global.cs
+++ src/user-tools/midi-console/Midi/Global.cs
@@ -20,6 +20,7 @@ global using Windows.Devices.Midi2.Diagnostics;
 global using Windows.Devices.Midi2.Enumeration;
 global using Windows.Devices.Midi2.Enumeration.Legacy;
 global using Windows.Devices.Midi2.Utilities.Messages;
+global using Windows.Devices.Midi2.Utilities.SysExTransfer;
 global using Windows.Devices.Midi2.Reporting;
 global using Windows.Devices.Midi2.Transports.BasicLoopback;
 global using Windows.Devices.Midi2.Transports.Loopback;
