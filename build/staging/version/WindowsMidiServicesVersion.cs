f4f9e63caeaad677dd36b43a61d0aff2edb4c5b6
Pete Brown <pete.brown@microsoft.com>
2026-08-23 23:55:53 -0400

    build system

--- build/staging/version/WindowsMidiServicesVersion.cs
+++ build/staging/version/WindowsMidiServicesVersion.cs
@@ -9,15 +9,15 @@ namespace Microsoft.Windows.Devices.Midi2.Common
 	{
 		public const bool IsPreview = true;
 		public const string Source = "GitHub Preview";
-		public const string BuildDate = "2026-08-16";
+		public const string BuildDate = "2026-08-23";
 		public const string Name = "Plugin Preview 1";
-		public const string BuildFullVersion = "1.0.0-preview.1.60";
+		public const string BuildFullVersion = "1.0.0-preview.1.62";
 		public const ushort VersionMajor = 1;
 		public const ushort VersionMinor = 0;
 		public const ushort VersionPatch = 0;
-		public const ushort VersionBuildNumber = 60;
-		public const string Preview = "preview.1.60";
-		public const string AssemblyFullVersion = "1.0.0.60";
-		public const string FileFullVersion = "1.0.0.60";
+		public const ushort VersionBuildNumber = 62;
+		public const string Preview = "preview.1.62";
+		public const string AssemblyFullVersion = "1.0.0.62";
+		public const string FileFullVersion = "1.0.0.62";
 	}
 }
