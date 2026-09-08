613820eb34fadcd86b04ac55cb686d12332f8e57
Pete Brown <pete.brown@microsoft.com>
2026-09-07 20:49:35 -0400

    Remove commented-out default value which was resulting in confusing intellisense

--- src/in-box/Inc/MidiDefs.h
+++ src/in-box/Inc/MidiDefs.h
@@ -116,7 +116,7 @@ static_assert(    MAXIMUM_LOOPED_BUFFER_SIZE < ULONG_MAX/2, "The maximum looped 
 
 // this is the amount of time we allocate to discovery across all native UMP format MIDI 2 devices. Needs to fit within uint16_t
 #define MIDI_DISCOVERY_TIMEOUT_REG_VALUE                L"Midi2DiscoveryTimeoutMS"
-//#define MIDI_DISCOVERY_TIMEOUT_DEFAULT_VALUE            10000
+
 #define MIDI_DISCOVERY_TIMEOUT_DEFAULT_VALUE            5000
 #define MIDI_DISCOVERY_TIMEOUT_MINIMUM_VALUE            500
 #define MIDI_DISCOVERY_TIMEOUT_MAXIMUM_VALUE            50000
